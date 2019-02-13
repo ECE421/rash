99134b718713eb7d48f05155ff5c9efb23751420
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-13 12:49:01 -0700

    rebuilding tests format

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
+require 'test/unit'
+
 require 'simplecov'
 SimpleCov.start
 
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
