6464f85a63258f75cc32104f92ed6f5937654b32
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-13 14:32:10 -0700

    adding simple `exit` command test

--- test/cmd_test.rb
+++ test/cmd_test.rb
@@ -1,5 +1,6 @@
 require_relative 'test_helper'
 require_relative '../lib/cmd'
+require 'mocha/test_unit'
 
 class CmdTest < Test::Unit::TestCase
   def setup
@@ -13,5 +14,11 @@ class CmdTest < Test::Unit::TestCase
   def test_initialization
     shell = Cmd.new
     assert_true(shell.is_a?(Cmd))
+  end
+
+  def test_exit
+    shell = Cmd.new
+    Readline.expects(:readline).returns('exit')
+    shell.cmd_loop
   end
 end
