178b9f4624a6e2a178c20ab526c4bb72aeca8f76
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-13 12:32:33 -0700

    adding basic command shell loop structure

---
+++ assignment2_main.rb
@@ -0,0 +1,13 @@
+# This Library was made by:
+#
+# Group 4:
+#   Nathan Klapstein (1449872)
+#   Tony Qian (1396109)
+#   Thomas Lorincz (1461567)
+#   Zach Drever (1446384)
+#
+
+require_relative 'lib/cmd'
+
+shell = Cmd.new
+shell.cmd_loop
