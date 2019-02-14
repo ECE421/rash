91536f88787f6d66b12edfd4dc7f67d17e9f34d5
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-14 08:23:35 -0700

    improving doc format

--- lib/cmd.rb
+++ lib/cmd.rb
@@ -1,6 +1,7 @@
 require 'readline'
 
-# A simple command shell implementation that is inspired from pythons cmd.py
+# A simple command shell implementation that is inspired
+# from Python's +cmd.py+.
 class Cmd
   def initialize(prompt = 'rash> ',
                  welcome = 'Welcome to the base ruby Cmd shell.
@@ -63,8 +64,8 @@ Type `help` for a list of available commands.', add_hist = true)
 
   # Hook to execute the unknown/unsupported command input.
   #
-  # return true to exit the cmd_loop
-  # return false to continue the cmd_loop
+  # return +true+ to exit the +cmd_loop+
+  # return +false+ to continue the +cmd_loop+
   def on_unknown_cmd(input)
     puts('WARNING: Unknown command: ' + input + ' reverting to system shell')
     system(input)
@@ -81,18 +82,18 @@ Type `help` for a list of available commands.', add_hist = true)
   # Default commands #
   ####################
 
-  # Note: command method definition is similar to pythons `cmd.py` implementation.
-  # * `do_foo` defines a command method for the command named "foo"
-  # * `help_foo` defines a help print command method for the command named "foo"
+  # Note: command method definition is similar to pythons +cmd.py+ implementation.
+  # * +do_foo+ defines a command method for the command named +foo+
+  # * +help_foo+ defines a help print command method for the command named +foo+
   #
-  # Note: if a `do_<command name>` method returns true it will terminate
-  # the cmd_loop.
+  # Note: if a +do_<command_name>+ method returns true it will terminate
+  # the +cmd_loop+.
 
   def help_exit(input)
     puts('Exit the command shell')
   end
 
-  # Exit the command loop by returning true.
+  # Exit the +cmd_loop+ by returning +true+.
   def do_exit(input)
     true
   end
@@ -125,14 +126,14 @@ Type `help` for a list of available commands.', add_hist = true)
   end
 
   def do_help(input)
-    if input == 'help' # basic `help` command
+    if input == 'help' # basic +help+ command
       puts('Use `help <command name>` to get help on a specific command')
       puts('Below is a list of available commands:')
       method_commands = self.class.instance_methods(false).select { |s| s.to_s.start_with?('do_') }
       method_commands.each do |method_symbol|
         puts(method_symbol.to_s[3..-1])
       end
-    else # advanced `help <command name>` command
+    else # advanced +help <command_name>+ command
       target_command = input[5..-1]
       if !(target_command_help_method = get_command_help_method_symbol(target_command)).nil?
         send(target_command_help_method, input)
@@ -144,11 +145,11 @@ Type `help` for a list of available commands.', add_hist = true)
   end
 
   ######
-  # Dynamic `do_<command name>` and `help_<command name>` utilities
+  # Dynamic +do_<command_name>+ and +help_<command_name>+ utilities
   ######
 
   # Given a command name obtain the symbol representing its
-  # corresponding `do_<command name>` method.
+  # corresponding +do_<command_name>+ method.
   def get_command_method_symbol(input)
     return if input.nil?
 
@@ -156,7 +157,7 @@ Type `help` for a list of available commands.', add_hist = true)
   end
 
   # Given a command name obtain the symbol representing its
-  # corresponding `help_<command name>` method.
+  # corresponding +help_<command_name>+ method.
   def get_command_help_method_symbol(input)
     return if input.nil?
 
