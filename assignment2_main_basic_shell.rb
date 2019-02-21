31a27d75ae4c2be23fec3788ac4bf3e453a6caba
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-21 12:07:39 -0700

    adding a basic shell skeleton

---
+++ assignment2_main_basic_shell.rb
@@ -0,0 +1,16 @@
+# Run a Ruby built basic shell.
+#
+# This Library was made by:
+#
+# Group 4:
+#   Nathan Klapstein (1449872)
+#   Tony Qian (1396109)
+#   Thomas Lorincz (1461567)
+#   Zach Drever (1446384)
+#
+
+require 'cmd'
+require 'basic_shell'
+
+shell = BasicShell.new
+shell.cmd_loop
---
+++ lib/basic_shell.rb
@@ -0,0 +1,96 @@
+require 'fileutils'
+
+require 'cmd'
+
+# A simple bash like shell made with rash/cmd.rb
+class BasicShell < Cmd
+  def initialize(prompt = 'rashbs> ',
+                 welcome = 'Welcome to the Ruby basic shell.
+Type `help` for a list of available commands.')
+    @prompt = prompt
+    @welcome = welcome
+
+    init_line_reader
+  end
+
+  # Print the current working DIRECTORY.
+  def do_pwd(arg)
+    puts Dir.pwd
+  end
+
+  # List the filenames within a DIRECTORY.
+  #
+  # If no DIRECTORY is specified the current working DIRECTORY will be listed.
+  def do_ls(arg)
+    if arg == ''
+      puts Dir.entries(Dir.pwd)
+    else
+      puts Dir.entries(arg)
+      # TODO: error handling
+    end
+  end
+
+  # Change the current working DIRECTORY.
+  def do_cd(arg)
+    Dir.chdir(arg)
+    # TODO: error handling
+    false
+  end
+
+  # Create one or more DIRECTORIES.
+  def do_mkdir(arg)
+    FileUtils.mkdir arg.split(' ')
+    false
+  end
+
+  # Removes one or more DIRECTORIES.
+  def do_rmdir(arg)
+    FileUtils.rmdir arg.split(' ')
+    false
+  end
+
+  # Remove one or more FILE(s).
+  def do_rm(arg)
+    FileUtils.rm arg.split(' ')
+    # TODO: error handling
+    false
+  end
+
+  # Move a FILE to the specified path.
+  def do_mv(arg)
+    args = arg.split(' ')
+    FileUtils.mv args[0], args[1]
+    # TODO: error handling
+    false
+  end
+
+  # Copy a FILE to the specified path.
+  def do_cp(arg)
+    args = arg.split(' ')
+    FileUtils.cp args[0], args[1]
+    # TODO: error handling
+    false
+  end
+
+  # Concatenate FILE(s) to standard output.
+  def do_cat(arg)
+    args = arg.split(' ')
+    args.each do |file|
+      File.open(file).readlines.each(&method(:puts))
+    end
+    false
+  end
+
+  # Update the access and modification times of each FILE to the current time.
+  #
+  # A FILE argument that does not exist is created empty.
+  def do_touch(arg)
+    args = arg.split(' ')
+    args.each do |file|
+      File.open(file, mode = 'w') # rubocop:disable Lint/UselessAssignment:
+    end
+    false
+  end
+
+  # TODO: need some method to write to files (maybe implement the > operator?)
+end
