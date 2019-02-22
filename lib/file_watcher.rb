9a5f9f73d9727528ae45795219efebf0c83f835d
Thomas Lorincz <tlorincz@ualberta.ca>
2019-02-22 15:38:09 -0700

    Setting up for snapshot comparison

--- lib/file_watcher.rb
+++ lib/file_watcher.rb
@@ -14,22 +14,19 @@ Type `help` for a list of available commands.')
   end
 
   def do_watch(args_string)
-    behaviour, duration, *filenames = args_string.split(' ')
+    behaviour, action, duration, *filenames = args_string.split(' ')
 
     if @c_behaviours.include?(behaviour)
-      thread = Thread.start { watch_create(duration.to_i, filenames) }
-      puts("Started thread: #{thread}")
+      thread = Thread.start { watch_create(filenames, action, duration.to_i) }
       @threads.push(thread)
     elsif @a_behaviours.include?(behaviour)
-      thread = Thread.start { watch_alter(duration.to_i, filenames) }
-      puts("Started thread: #{thread}")
+      thread = Thread.start { watch_alter(filenames, action, duration.to_i) }
       @threads.push(thread)
     elsif @d_behaviours.include?(behaviour)
-      thread = Thread.start { watch_delete(duration.to_i, filenames) }
-      puts("Started thread: #{thread}")
+      thread = Thread.start { watch_delete(filenames, action, duration.to_i) }
       @threads.push(thread)
     else
-      puts("Invalid behaviour #{behaviour}. Please use one of the following behaviours:")
+      puts("Invalid behaviour: '#{behaviour}'. Please use watch with one of the following behaviours:")
       puts("\tCreate: #{@c_behaviours}")
       puts("\tAlter/Modify: #{@a_behaviours}")
       puts("\tDelete: #{@d_behaviours}")
@@ -39,42 +36,53 @@ Type `help` for a list of available commands.')
   end
 
   def help_watch(_)
-    puts('Usage: watch [BEHAVIOUR] [DURATION] *[FILENAMES]')
+    puts('Usage: watch [BEHAVIOUR] [ACTION] [DURATION] *[FILENAMES]')
     puts('Description: Watch files denoted by FILENAMES for BEHAVIOUR.')
   end
 
   private
 
-  def watch_create(duration, filenames)
-    while true
-      filenames.each do |_|
-        nil
-      end
+  def watch_create(filenames, action, duration)
+    last_snapshots = {}
+    filenames.each do |file|
+      last_snapshots[file] = snapshot
     end
+
+    # TODO
+    action_after_change(action, duration)
   end
 
-  def watch_alter(duration, filenames)
-    while true
-      filenames.each do |_|
-        nil
-      end
+  def watch_alter(filenames, action, duration)
+    last_snapshots = {}
+    filenames.each do |file|
+      last_snapshots[file] = snapshot
     end
+
+    # TODO
+    action_after_change(action, duration)
   end
 
-  def watch_delete(duration, filenames)
-    while true
-      filenames.each do |_|
-        nil
-      end
+  def watch_delete(filenames, action, duration)
+    last_snapshots = {}
+    filenames.each do |file|
+      last_snapshots[file] = snapshot
     end
+
+    # TODO
+    action_after_change(action, duration)
   end
 
-  def action_after_change(duration, action = "puts('Hello, security vulnerability!')")
+  def action_after_change(action, duration)
+    # Hello, security vulnerability!
     sleep(duration)
     proc {
       $SAFE = 4
       eval(action)
     }.call
+  end
+
+  def snapshot
+    # TODO
   end
 
   def post_loop
