08ed4198ca50e798553d8b50867a5f24dd24b0b2
Thomas Lorincz <tlorincz@ualberta.ca>
2019-02-22 22:18:40 -0700

    Added simple routines for monitoring file create/alter/delete

--- lib/file_watcher.rb
+++ lib/file_watcher.rb
@@ -1,4 +1,4 @@
-require_relative 'cmd'
+require 'cmd'
 
 # A simple shell implementation of a file watcher program in Ruby
 class FileWatcher < Cmd
@@ -43,46 +43,65 @@ Type `help` for a list of available commands.')
   private
 
   def watch_create(filenames, action, duration)
-    last_snapshots = {}
     filenames.each do |file|
-      last_snapshots[file] = snapshot
+      next unless File.exist?(file)
+
+      filenames.pop(file)
     end
 
-    # TODO
-    action_after_change(action, duration)
+    created = []
+    until created.eql?(filenames)
+      filenames.each do |file|
+        next unless File.exist?(file)
+
+        created.push(file)
+        action_after_change(action, duration)
+      end
+    end
   end
 
   def watch_alter(filenames, action, duration)
     last_snapshots = {}
     filenames.each do |file|
-      last_snapshots[file] = snapshot
+      abort unless File.exist?(file)
+      last_snapshots[file] = File.mtime(file)
     end
 
-    # TODO
-    action_after_change(action, duration)
+    until last_snapshots.empty?
+      last_snapshots.each do |file, last_snapshot|
+        next unless (File.mtime(file) <=> last_snapshot).positive?
+
+        # File has been modified
+        last_snapshots.delete(file)
+        action_after_change(action, duration)
+      end
+    end
   end
 
   def watch_delete(filenames, action, duration)
-    last_snapshots = {}
     filenames.each do |file|
-      last_snapshots[file] = snapshot
+      next if File.exist?(file)
+
+      filenames.pop(file)
     end
 
-    # TODO
-    action_after_change(action, duration)
+    deleted = []
+    until deleted.eql?(filenames)
+      filenames.each do |file|
+        next if File.exist?(file)
+
+        deleted.push(file)
+        action_after_change(action, duration)
+      end
+    end
   end
 
   def action_after_change(action, duration)
     # Hello, security vulnerability!
     sleep(duration)
     proc {
-      $SAFE = 4
       eval(action)
     }.call
-  end
-
-  def snapshot
-    # TODO
   end
 
   def post_loop
