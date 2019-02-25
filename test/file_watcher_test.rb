b5f902aa7b2cae8cea32f5ff8780425ded7d2b29
Nathan Klapstein <nklapste@ualberta.ca>
2019-02-25 14:06:12 -0700

    adding smokes

--- test/file_watcher_test.rb
+++ test/file_watcher_test.rb
@@ -14,18 +14,27 @@ class FileWatcherTest < Test::Unit::TestCase
     assert_true(@shell.is_a?(Cmd))
   end
 
-  def test_watch_create
-    # TODO
-    true
+  def test_smoke_watch_create
+    test_file = create_tempfile_test_file('test_file', 'test content')
+
+    Readline.stubs(:readline)
+            .returns('watch create exit 1 ' + test_file.path.to_s, 'exit')
+    @shell.cmd_loop
   end
 
-  def test_watch_alter
-    # TODO
-    true
+  def test_smoke_watch_alter
+    test_file = create_tempfile_test_file('test_file', 'test content')
+
+    Readline.stubs(:readline)
+            .returns('watch alter print 1 ' + test_file.path.to_s, 'exit')
+    @shell.cmd_loop
   end
 
-  def test_watch_delete
-    # TODO
-    true
+  def test_smoke_watch_delete
+    test_file = create_tempfile_test_file('test_file', 'test content')
+
+    Readline.stubs(:readline)
+            .returns('watch delete print 1 ' + test_file.path.to_s, 'exit')
+    @shell.cmd_loop
   end
 end
